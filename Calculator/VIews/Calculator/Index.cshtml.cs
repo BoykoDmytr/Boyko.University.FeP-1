6adc48b86f4dd6a41d874298e2f3ed66ab3a45a3
Дмитро Бойко <dmytro.boiko@lnu.edu.ua>
2024-03-10 15:23:02 +0200

    Week 2(1)

---
+++ Calculator/VIews/Calculator/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Calculator.VIews.Calculator
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
